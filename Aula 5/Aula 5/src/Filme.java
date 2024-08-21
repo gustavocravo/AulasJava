4ba8a1d23e8a63d5b20ebf47c5e175b48cbbd319
gustavocravo <gustavocravo.profissional@gmail.com>
2024-08-21 14:00:04 -0300

    Continuacao projeto Filmes

--- Aula 5/Aula 5/src/Filme.java
+++ Aula 5/Aula 5/src/Filme.java
@@ -2,8 +2,21 @@ public class Filme {
    String nome;
    int anoLancamento;
    boolean incluidoNoPlano;
-   double avaliacao;
+   double somaAvaliacao;
    int totalAvaliacoes;
    int duracaoMinutos;
-   
+
+   void exibeFichaTecnica() {
+      System.out.println("Nome do filme: " + nome);
+      System.out.println("Ano de lançamento: " + anoLancamento);
+   }
+
+   void avalia(double nota) {
+      somaAvaliacao += nota;
+      totalAvaliacoes++;
+   }
+
+   double pegaMedia() {
+      return somaAvaliacao / totalAvaliacoes;
+   }
 }
--- Aula 5/Aula 5/src/Principal.java
+++ Aula 5/Aula 5/src/Principal.java
@@ -1,5 +1,17 @@
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefao";
+        meuFilme.anoLancamento = 1970;
+        meuFilme.duracaoMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println(meuFilme.somaAvaliacao);
+        System.out.println(meuFilme.totalAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
     }
 }
